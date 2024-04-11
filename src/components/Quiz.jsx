dc40e8436a18389b2c6f8724e156901c88a5ba85
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2024-04-11 10:55:14 +0300

    Show current question and select answers.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import { shuffleArray } from "../utils.js";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const currentQuestionIndex = userAnswers.length;
-  return <h2>{QUESTIONS[currentQuestionIndex].text}</h2>;
+  const question = QUESTIONS[currentQuestionIndex];
+  let shuffledAnswers = [...question.answers];
+  shuffleArray(shuffledAnswers);
+
+  const handleSelectedAnswer = (answer) => {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, answer];
+    });
+  };
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{question.text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectedAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
